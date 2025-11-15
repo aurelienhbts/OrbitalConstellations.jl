faec36b78fef62d27226626dc663a9a1d404ed22
Aurélien Habets <aurelienhabets@duck.com>
2025-11-15 22:13:43 +0100

    Create SatsLEO module

---
+++ SatsLEO/SatsLEO.jl
@@ -0,0 +1,15 @@
+module SatsLEO
+
+using LinearAlgebra, Plots
+
+include("basis.jl")
+export  μ, Re, ωe, Sat, deg2rad, rad2deg, R1, R3, 
+        ecef_from_eci, latlon_from_ecef, eci_pos, walker_delta, myconstellation
+
+include("calculations.jl")
+export  visible, coverage_fraction, mean_coverage_fraction, eval_constellation
+
+include("visualisation.jl")
+export show_coverage_heatmap, plot_constellation!, plot_earth
+
+end
